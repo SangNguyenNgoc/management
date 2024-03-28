35a866e08e38d14755ee87a4382880d01da4f82b
Thanh Trong Vo <votrong1471@gmail.com>
2024-03-28 13:04:03 +0700

    Cập nhật CRUD device

--- src/main/java/com/example/markethibernate/bll/services/DeviceService.java
+++ src/main/java/com/example/markethibernate/bll/services/DeviceService.java
@@ -1,5 +1,8 @@
 package com.example.markethibernate.bll.services;
 
+import com.example.markethibernate.dal.dao.DeviceDao;
+import com.example.markethibernate.dal.entities.DeviceEntity;
+
 public class DeviceService {
 
     private static class DeviceServiceHolder {
@@ -12,4 +15,24 @@ public class DeviceService {
     public static DeviceService getInstance() {
         return DeviceServiceHolder.INSTANCE;
     }
+
+    public void deleteDevice(DeviceEntity device) {
+        if (device != null) {
+            DeviceDao.getInstance().deleteDevice(device);
+        }
+
+    }
+
+    public void updateDevice(DeviceEntity device) {
+        if (device != null) {
+            DeviceDao.getInstance().updateDevice(device);
+        }
+
+    }
+    public void createDevice(DeviceEntity device) {
+        if (device != null) {
+            DeviceDao.getInstance().save(device);
+        }
+    }
+
 }
--- src/main/java/com/example/markethibernate/dal/dao/DeviceDao.java
+++ src/main/java/com/example/markethibernate/dal/dao/DeviceDao.java
@@ -3,6 +3,9 @@ package com.example.markethibernate.dal.dao;
 import com.example.markethibernate.dal.entities.DeviceEntity;
 import com.example.markethibernate.utils.HibernateUtil;
 import jakarta.persistence.Query;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaDelete;
+import jakarta.persistence.criteria.Root;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -49,6 +52,26 @@ public class DeviceDao {
             }
         }
     }
+//    Xoa theo dieu kien
+    public boolean deleteDevicesByCondition(String condition) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            CriteriaBuilder builder = session.getCriteriaBuilder();
+            CriteriaDelete<DeviceEntity> criteriaDelete = builder.createCriteriaDelete(DeviceEntity.class);
+            Root<DeviceEntity> root = criteriaDelete.from(DeviceEntity.class);
+            criteriaDelete.where(builder.equal(root.get(condition), condition));
+            int result = session.createQuery(criteriaDelete).executeUpdate();
+            transaction.commit();
+            return result > 0;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.log(Level.SEVERE, "Failed to delete devices by condition", e);
+            throw new RuntimeException("Failed to delete devices by condition", e);
+        }
+    }
 
     public DeviceEntity save(DeviceEntity device) {
         Transaction transaction = null;
@@ -64,6 +87,60 @@ public class DeviceDao {
             }
             logger.log(Level.SEVERE, "Query failure");
             return null;
+        }
+    }
+    public DeviceEntity createDevice(DeviceEntity device) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.save(device);
+            transaction.commit();
+            return device;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.log(Level.SEVERE, "Failed to create device", e);
+            return null;
+        }
+    }
+    public boolean deleteDevice(DeviceEntity device) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.delete(device);
+            transaction.commit();
+            return true;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.log(Level.SEVERE, "Failed to delete device", e);
+            return false;
+        }
+    }
+
+//    Xoa theo id
+    public boolean deleteDeviceById(int id) {
+        DeviceEntity device = findById(id);
+        if (device != null) {
+            return deleteDevice(device);
+        }
+        return false;
+    }
+    public DeviceEntity updateDevice(DeviceEntity device) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.update(device);
+            transaction.commit();
+            return device;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            logger.log(Level.SEVERE, "Failed to update device", e);
+            throw new RuntimeException("Failed to update device", e);
         }
     }
 }
