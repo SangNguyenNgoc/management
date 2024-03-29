be25ec32e6de15721f044873ec2e475efd9aa038
tungd <tungduong2288@gmail.com>
2024-03-29 10:25:46 +0700

    manage person

--- src/main/java/com/example/markethibernate/bll/services/PersonService.java
+++ src/main/java/com/example/markethibernate/bll/services/PersonService.java
@@ -1,5 +1,8 @@
 package com.example.markethibernate.bll.services;
 
+import com.example.markethibernate.dal.dao.PersonDao;
+import com.example.markethibernate.dal.entities.PersonEntity;
+
 public class PersonService {
 
     private static class PersonServiceHolder {
@@ -12,4 +15,24 @@ public class PersonService {
     public static PersonService getInstance() {
         return PersonServiceHolder.INSTANCE;
     }
+
+    public PersonEntity addPerson(PersonEntity person) {
+        if (person == null) {
+            return null;
+        }
+        return PersonDao.getInstance().addPerson(person);
+    }
+    public PersonEntity updatePerson(PersonEntity person) {
+        if (person == null) {
+            return null;
+        }
+        return PersonDao.getInstance().updatePerson(person);
+    }
+    public PersonEntity deletePersonById(int id) {
+        if (id == 0) {
+            return null;
+        }
+        return PersonDao.getInstance().deletePersonById(id);
+    }
+
 }
