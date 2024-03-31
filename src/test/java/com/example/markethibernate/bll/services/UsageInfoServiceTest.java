fe2ec61f1936d40919bea26ae4c5f316c3ae13bc
notobo15 <binh15122003@gmail.com>
2024-03-31 16:22:43 +0700

    test: usageInfo dao and services layer

--- src/test/java/com/example/markethibernate/bll/services/UsageInfoServiceTest.java
+++ src/test/java/com/example/markethibernate/bll/services/UsageInfoServiceTest.java
@@ -1,5 +1,6 @@
-package com.example.markethibernate.dal.dao;
+package com.example.markethibernate.bll.services;
 
+import com.example.markethibernate.dal.dao.UsageInfoDao;
 import com.example.markethibernate.dal.entities.DeviceEntity;
 import com.example.markethibernate.dal.entities.PersonEntity;
 import com.example.markethibernate.dal.entities.UsageInfoEntity;
@@ -8,15 +9,15 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.List;
 
-public class UsageInfoDaoTest {
+public class UsageInfoServiceTest {
     @Test
     void findAll() {
-        List<UsageInfoEntity> list = UsageInfoDao.getInstance().findAll();
+        List<UsageInfoEntity> list = UsageInfoService.getInstance().findAll();
         list.forEach(item -> System.out.println(item.toString()));
     }
     @Test
     void findById() {
-        UsageInfoEntity usageInfo = UsageInfoDao.getInstance().findById(5L);
+        UsageInfoEntity usageInfo = UsageInfoService.getInstance().findById(2L);
         System.out.println(usageInfo);
     }
     @Test
@@ -29,27 +30,26 @@ public class UsageInfoDaoTest {
                 .device(DeviceEntity.builder().id(1000001L).build())
                 .build();
         System.out.println("Pre save: " + usageInfo.toString());
-        usageInfo = UsageInfoDao.getInstance().save(usageInfo);
+        usageInfo = UsageInfoService.getInstance().save(usageInfo);
         System.out.println("After save:" + usageInfo.toString());
     }
     @Test
     void update() {
         UsageInfoEntity usageInfo = UsageInfoEntity.builder()
-                .id(4L)
+                .id(5L)
                 .borrowTime(LocalDateTime.now())
                 .checkinTime(LocalDateTime.now())
                 .returnTime(LocalDateTime.now())
-                .person(PersonEntity.builder().id(1L).build())
+                .person(PersonEntity.builder().id(5L).build())
                 .device(DeviceEntity.builder().id(1000001L).build())
                 .build();
         System.out.println("Pre update: " + usageInfo.toString());
-        usageInfo = UsageInfoDao.getInstance().update(usageInfo);
+        usageInfo = UsageInfoService.getInstance().update(usageInfo);
         System.out.println("After update:" + usageInfo.toString());
     }
 
     @Test
     void deleteById() {
-        UsageInfoDao.getInstance().deleteById(1L);
+        UsageInfoService.getInstance().deleteById(5L);
     }
-
 }
