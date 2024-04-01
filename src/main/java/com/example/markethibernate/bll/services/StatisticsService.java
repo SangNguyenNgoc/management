e8e5ad9df0b85407118b9a0b57d1ed42490160ab
notobo15 <binh15122003@gmail.com>
2024-04-01 12:36:22 +0700

    chorce: implement statistics service layer

--- src/main/java/com/example/markethibernate/bll/services/StatisticsService.java
+++ src/main/java/com/example/markethibernate/bll/services/StatisticsService.java
@@ -1,11 +1,13 @@
 package com.example.markethibernate.bll.services;
 import com.example.markethibernate.dal.dao.UsageInfoDao;
+import com.example.markethibernate.dal.entities.PenalizeEntity;
 import com.example.markethibernate.utils.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 public class StatisticsService {
     private final SessionFactory sessionFactory;
@@ -18,11 +20,10 @@ public class StatisticsService {
     private StatisticsService() {
         this.sessionFactory = HibernateUtil.getSessionFactory();
     }
-    public Long countDevicesBorrowedBetween(LocalDateTime startTime, LocalDateTime endTime) {
-        Transaction transaction = null;
+    public Long countPeopleCheckInBetween(LocalDateTime startTime, LocalDateTime endTime) {
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
-            Query<Long> query = session.createQuery("SELECT COUNT(DISTINCT usage.device.id) " +
+            session.beginTransaction();
+            Query<Long> query = session.createQuery("SELECT COUNT(DISTINCT usage.person.id) " +
                     "FROM UsageInfoEntity usage " +
                     "WHERE usage.borrowTime BETWEEN :startTime AND :endTime OR " +
                     "(usage.borrowTime < :endTime AND usage.returnTime IS NULL)", Long.class);
@@ -36,10 +37,9 @@ public class StatisticsService {
 
     }
 
-    public Long countDevicesBorrowedBetween2(LocalDateTime startTime, LocalDateTime endTime) {
-        Transaction transaction = null;
+    public Long countDevicesBorrowedBetween(LocalDateTime startTime, LocalDateTime endTime) {
         try (Session session = sessionFactory.openSession()) {
-            transaction = session.beginTransaction();
+            session.beginTransaction();
             Query<Long> query = session.createQuery("SELECT COUNT(DISTINCT usage.device.id) " +
                     "FROM UsageInfoEntity usage " +
                     "WHERE (usage.borrowTime BETWEEN :startTime AND :endTime) OR " +
@@ -47,24 +47,23 @@ public class StatisticsService {
             query.setParameter("startTime", startTime);
             query.setParameter("endTime", endTime);
             Long result = query.uniqueResult();
-            transaction.commit();
             return result;
         } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
             e.printStackTrace();
             return null;
         }
     }
-    public Long countDevicesBorrowedAndNotReturned(LocalDateTime currentTime) {
+
+    public Long countDevicesBorrowedAndNotReturned(LocalDateTime startTime, LocalDateTime endTime) {
         try (Session session = sessionFactory.openSession()) {
             String hql = "SELECT COUNT(DISTINCT usage.device.id) " +
                     "FROM UsageInfoEntity usage " +
-                    "WHERE usage.borrowTime <= :currentTime " +
-                    "AND (usage.returnTime IS NULL OR usage.returnTime > :currentTime)";
+                    "WHERE usage.borrowTime BETWEEN :startTime AND :endTime " +
+                    "AND (usage.returnTime IS NULL OR usage.returnTime > :endTime)" +
+                    "AND usage.returnTime IS NULL";
             Query<Long> query = session.createQuery(hql, Long.class);
-            query.setParameter("currentTime", currentTime);
+            query.setParameter("startTime", startTime);
+            query.setParameter("endTime", endTime);
             return query.uniqueResult();
         } catch (Exception e) {
             e.printStackTrace();
@@ -83,6 +82,18 @@ public class StatisticsService {
         } catch (Exception e) {
             e.printStackTrace();
             return null;
+        }
+    }
+
+    public List<PenalizeEntity> getPenaltiesByStatus(boolean processed) {
+        try (Session session = sessionFactory.openSession()) {
+            String hql = "FROM PenalizeEntity p WHERE p.status = :processed";
+            Query<PenalizeEntity> query = session.createQuery(hql, PenalizeEntity.class);
+            query.setParameter("processed", processed);
+            return query.list();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ArrayList<>();
         }
     }
 }
