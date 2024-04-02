597be0e181ee11a899f967ccde01f570cd736660
ngocson741852963 <117715756+ngocson741852963@users.noreply.github.com>
2024-04-02 16:26:40 +0700

    home layout

--- src/main/java/com/example/markethibernate/HelloApplication.java
+++ src/main/java/com/example/markethibernate/HelloApplication.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,8 +13,10 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 700);
         stage.setTitle("Hello!");
+        stage.initStyle(StageStyle.DECORATED);
+        stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/com/example/markethibernate/gui/HelloController.java
+++ src/main/java/com/example/markethibernate/gui/HelloController.java
@@ -1,18 +1,75 @@
 package com.example.markethibernate.gui;
 
+import com.example.markethibernate.HelloApplication;
 import com.example.markethibernate.utils.HibernateUtil;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 
-public class HelloController {
-    @FXML
-    private Label welcomeText;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
+public class HelloController implements Initializable {
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView personImage;
+    @FXML
+    private ImageView deviceImage;
+    @FXML
+    private ImageView penaltyImage;
+    @FXML
+    private ImageView usageImage;
+    @FXML
+    private Button personButton;
+    @FXML
+    private Button deviceButton;
+    @FXML
+    private Button penaltyButton;
+    @FXML
+    private Button usageButton;
+
+    private void handleButtonClick(Button button, ImageView imageView, String imageUrl) {
+        // Xử lý sự kiện khi nhấp vào nút
+        resetButtonStyles();
+        button.getStyleClass().add("layout-button-isSelect");
+        imageView.setImage(new Image(HelloApplication.class.getResource(imageUrl).toString()));
+
+    }
+
+    private void resetButtonStyles() {
+        // Thiết lập lại lớp của tất cả các nút về mặc định
+        personButton.getStyleClass().clear();
+        deviceButton.getStyleClass().clear();
+        penaltyButton.getStyleClass().clear();
+        usageButton.getStyleClass().clear();
+
+        personButton.getStyleClass().add("layout-button");
+        deviceButton.getStyleClass().add("layout-button");
+        penaltyButton.getStyleClass().add("layout-button");
+        usageButton.getStyleClass().add("layout-button");
+
+        personImage.setImage(new Image(HelloApplication.class.getResource("image/person.png").toString()));
+        deviceImage.setImage(new Image(HelloApplication.class.getResource("image/device.png").toString()));
+        penaltyImage.setImage(new Image(HelloApplication.class.getResource("image/penalty.png").toString()));
+        usageImage.setImage(new Image(HelloApplication.class.getResource("image/usage.png").toString()));
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        // Bắt sự kiện khi khởi tạo controller
+        personButton.setOnAction(event -> handleButtonClick(personButton, personImage, "image/person-active.png"));
+        deviceButton.setOnAction(event -> handleButtonClick(deviceButton, deviceImage, "image/device-active.png"));
+        penaltyButton.setOnAction(event -> handleButtonClick(penaltyButton, penaltyImage, "image/penalty-active.png"));
+        usageButton.setOnAction(event -> handleButtonClick(usageButton, usageImage, "image/usage-active.png"));
     }
 }
